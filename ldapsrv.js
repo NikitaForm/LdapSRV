32c1270b56bea5d32adfc088e567c4f5dc08123a
NikitaForm <smolyakov.nikita@gmail.com>
2014-01-27 02:00:36 +0200

    Эксперимент 4

--- ldapsrv.js
+++ ldapsrv.js
@@ -69,7 +69,6 @@ server.search(SUFFIX, function(req, res, next) {
     return next();
 });
 
-server.listen(process.env.PORT || 1389, 'sleepy-caverns-7803.herokuapp.com', function() {
+server.listen(process.env.PORT || 1389, 'sleepy-caverns-7803dfgdfg', function() {
     console.log('LDAP server up at: %s', server.url);
-    console.log(ENV['URL'] + '    1321321312');
 });
