35aeb0a4c3a65d75cea6e6b983b8176e5b292d08
NikitaForm <smolyakov.nikita@gmail.com>
2014-02-04 23:34:32 +0200

    Эксперимент 6

--- ldapsrv.js
+++ ldapsrv.js
@@ -70,7 +70,7 @@ server.search(SUFFIX, function(req, res, next) {
     return next();
 });
 
-server.listen(process.env.PORT || 1389, function() {
+server.listen(process.env.PORT || 1389, '127.0.0.1', function() {
     console.log('LDAP server up at: %s', server.url);
     console.log(JSON.stringify(server.address()));
 });
